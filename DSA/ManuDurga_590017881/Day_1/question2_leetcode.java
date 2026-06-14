456a8aa015cf542ffc83a20c2700b84ab73c4573
ManuDurga <you@example.com>
2026-06-15 04:07:58 +0530

    Day 1 Revised Submission

--- DSA/ManuDurga_590017881/Day_1/question2_leetcode.java
+++ DSA/ManuDurga_590017881/Day_1/question2_leetcode.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 
 class question2_leetcode{
     public static void main(String[] args){
-        int missingNumberSum = 0;
-        int totalNumberSum=0;
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();          //this is the size of the input array
+        int [] a = new int[n];
         int[] nums = new int[n];
         for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();missingNumberSum=missingNumberSum+nums[i];}     //elements of that array
-        for(int j =0; j<=n;j++){totalNumberSum=totalNumberSum+(j+1);}
-        System.out.println(totalNumberSum-missingNumberSum);
+            nums[i] = sc.nextInt();}     //elements of that array
+        int arr1=0;
+        int arr2=0;
+        for(int i =0;i<n;i++){a[i]=i+1;
+        arr1=arr1^a[i];}
+        for (int j:nums){arr2=arr2^j;}
+        System.out.println(arr1^arr2); 
      }}
